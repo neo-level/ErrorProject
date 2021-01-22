7ab211aefc3746082cc60ed0176ab1a42d42d717
frederik desmet <info@neo-level.com>
2021-01-22 16:44:46 +0100

    no using of the generic for list. forgotten semicolon. float statement without the f.

--- Assets/CongratScript.cs
+++ Assets/CongratScript.cs
@@ -1,31 +1,32 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CongratScript : MonoBehaviour
 {
     public TextMesh Text;
     public ParticleSystem SparksParticles;
-    
+
     private List<string> TextToDisplay;
-    
+
     private float RotatingSpeed;
     private float TimeToNextText;
 
     private int CurrentText;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         TimeToNextText = 0.0f;
-        CurrentText = 0
-        
-        RotatingSpeed = 1.0;
+        CurrentText = 0;
+
+        RotatingSpeed = 1.0f;
 
         TextToDisplay.Add("Congratulation");
         TextToDisplay.Add("All Errors Fixed");
 
         Text.text = TextToDisplay[0];
-        
+
         SparksParticles.Play();
     }
 
@@ -37,14 +38,15 @@ public class CongratScript : MonoBehaviour
         if (TimeToNextText > 1.5f)
         {
             TimeToNextText = 0.0f;
-            
+
             CurrentText++;
             if (CurrentText >= TextToDisplay.Count)
             {
                 CurrentText = 0;
 
 
-            Text.text = TextToDisplay[CurrentText];
+                Text.text = TextToDisplay[CurrentText];
+            }
         }
     }
 }
